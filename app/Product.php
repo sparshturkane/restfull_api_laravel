9c183dc314b5d18cbcfa355eeb944ab28dbd0915
sparshturkane <rajaturkane99@gmail.com>
2017-08-03 13:48:47 +0530

    33 contants,fillables in product model

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const AVALIABLE_PRODUCT = 'avaliable';
+    const UNAVALIABLE_PRODUCT = 'unavaliable';
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    public function isAvaliable()
+    {
+        return $this->status == Product::AVALIABLE_PRODUCT;
+    }
 }
