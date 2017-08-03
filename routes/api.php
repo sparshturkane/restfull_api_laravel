7fc6b6e8b9ba296316a57afc2cdf16e3bea18f47
sparshturkane <rajaturkane99@gmail.com>
2017-08-03 12:24:56 +0530

    made initial models, controllers, route:resource(i still don't know what they are exactly) and done some migration

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,40 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+//
+// Route::get('/yea', function (){
+//     return "hello this is api";
+// });
+
+/**
+* Buyer
+*/
+Route::resource('buyers','Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+/**
+* Categories
+*/
+Route::resource('categories','Category\CategoryController', ['except' => ['create', 'edit']]);
+
+/**
+* Products
+*/
+Route::resource('products','Product\ProductController', ['only' => ['index', 'show']]);
+
+/**
+* Sellers
+*/
+Route::resource('sellers','Seller\SellerController', ['only' => ['index', 'show']]);
+
+/**
+* Transactions
+*/
+Route::resource('transactions','Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+/**
+* Users
+*/
+Route::resource('users','User\UserController', ['except' => ['create', 'edit']]);
