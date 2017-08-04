8a63623f697fa70cea1df8b445a293d9c2372372
sparshturkane <rajaturkane99@gmail.com>
2017-08-04 19:43:29 +0530

    49 get all users (/api/users)

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\User;
 class UserController extends Controller
 {
     /**
@@ -14,7 +14,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return response()->json(['data' => $users], 200);
     }
 
     /**
