dfbab8d3c11d696fc945b3892265b14d2ba316f3
sparshturkane <rajaturkane99@gmail.com>
2017-08-05 11:00:39 +0530

    there was some spelling mistake

--- app/User.php
+++ app/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
         'email',
         'password',
         'verified',
-        'verfication_token',
+        'verification_token',
         'admin',
     ];
 
