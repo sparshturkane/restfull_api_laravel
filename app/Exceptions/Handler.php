7c8fb8fad3790da3bc483a9cc053cc41f49c3615
sparshturkane <rajaturkane99@gmail.com>
2017-08-06 23:50:47 +0530

    unexpected Exception like db is down (server side stuff)

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -90,7 +90,13 @@ class Handler extends ExceptionHandler
                 return $this->errorResponse("Cannot remove this resource permanently. It is related with any other resource", 409);
             }
         }
-        return parent::render($request, $exception);
+
+        if (config('app.debug')) {
+            return parent::render($request, $exception);
+        }
+        
+        return $this->errorResponse("Unexpected Exception. Try Later", 500);
+
     }
 
     /**
