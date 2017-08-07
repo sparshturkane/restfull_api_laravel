b6ce601e6f623871c627a73621d124148a7daeda
sparshturkane <rajaturkane99@gmail.com>
2017-08-07 10:05:03 +0530

    using implicit model binding in UserController

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -61,9 +61,9 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        $user = User::findOrFail($id);
+        // $user = User::findOrFail($id); // removed because of implicit model binding
         // return response()->json(['data' => $user], 200);
         return $this->showOne($user);
     }
@@ -86,9 +86,9 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        $user = User::findOrFail($id);
+        // $user = User::findOrFail($id); // removed because of implicit model binding
         $rules = [
             'email' => 'email|unique:users,email,' . $user->id,
             'password' => 'min:6|confirmed',
@@ -135,9 +135,9 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        $user = User::findOrFail($id);
+        // $user = User::findOrFail($id); // removed because of implicit model binding
         $user->delete();
         // return response()->json(['data' => $user], 200);
         return $this->showOne($user);
