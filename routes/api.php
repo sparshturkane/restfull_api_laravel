82b4a3b745dae6c75f91d6570af872c0e1d331c7
sparshturkane <rajaturkane99@gmail.com>
2017-08-07 17:03:57 +0530

    105 ProductBuyerController index method (/api/products/{id}/buyers)get

--- routes/api.php
+++ routes/api.php
@@ -43,7 +43,8 @@ Route::resource('categories.buyers','Category\CategoryBuyerController', ['only' 
 * Products
 */
 Route::resource('products','Product\ProductController', ['only' => ['index', 'show']]);
-Route::resource('products.transactions','Product\ProductTransactionController', ['only' => ['index', 'show']]);
+Route::resource('products.transactions','Product\ProductTransactionController', ['only' => ['index']]);
+Route::resource('products.buyers','Product\ProductBuyerController', ['only' => ['index']]);
 
 /**
 * Sellers
