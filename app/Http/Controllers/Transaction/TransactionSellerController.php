b2068425b7f84b0b11b45c73875c19abf53ece3c
sparshturkane <rajaturkane99@gmail.com>
2017-08-07 12:11:23 +0530

    87 TransactionSellerController index method (/api/transactions/{}/sellers) get(gives the seller of that transaction)

---
+++ app/Http/Controllers/Transaction/TransactionSellerController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Transaction;
+
+use App\Transaction;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class TransactionSellerController extends ApiController
+{
+    /**
+     * List of all the seller in a perticular transaction.
+     * a transaction will have only one seller
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Transaction $transaction)
+    {
+        $seller = $transaction->product->seller;
+        return $this->showOne($seller);
+    }
+}
