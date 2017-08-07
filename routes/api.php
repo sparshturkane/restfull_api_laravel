1dc83abd92b658f3116adfe186fecbd720960167
sparshturkane <rajaturkane99@gmail.com>
2017-08-07 14:19:10 +0530

    98 sellerCategoryController method index (/api/sellers/2/categories)

--- routes/api.php
+++ routes/api.php
@@ -49,6 +49,7 @@ Route::resource('products','Product\ProductController', ['only' => ['index', 'sh
 */
 Route::resource('sellers','Seller\SellerController', ['only' => ['index', 'show']]);
 Route::resource('sellers.transactions','Seller\SellerTransactionController', ['only' => ['index']]);
+Route::resource('sellers.categories','Seller\SellerCategoryController', ['only' => ['index']]);
 
 /**
 * Transactions
